f0f40dad711c816db24db8b4ff7209cbe411594d
Ankush <rehalankush192@gmail.com>
2024-10-28 21:31:08 +0530

    socket updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://chat-now-y5c9.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
